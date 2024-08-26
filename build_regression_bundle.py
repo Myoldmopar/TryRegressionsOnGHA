c5f2250f1d3c3d49fbc2e7cb784b0f965a1d94df
Edwin Lee <leeed2001@gmail.com>
2024-08-26 15:37:58 -0500

    Tuning

--- build_regression_bundle.py
+++ build_regression_bundle.py
@@ -1,6 +1,9 @@
 from os import makedirs
+from sys import argv, exit
 
 makedirs('build/regressions', exist_ok=True)
 with open('build/regressions/index.html', 'w') as f:
     f.write('<html><h4>Hello, world</h4></html>')
 
+if argv[1] == 'macos-14':
+    exit(1)
