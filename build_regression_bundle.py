446b443e019ca44debf22cd941efdc2710db2114
Edwin Lee <leeed2001@gmail.com>
2024-08-26 13:32:24 -0500

    Proper path to regression bundle script

--- build_regression_bundle.py
+++ build_regression_bundle.py
@@ -1,6 +1,6 @@
 from os import makedirs
 
-makedirs('regressions', exist_ok=True)
-with open('regressions/index.html', 'w') as f:
+makedirs('build/regressions', exist_ok=True)
+with open('build/regressions/index.html', 'w') as f:
     f.write('<html><h4>Hello, world</h4></html>')
 
