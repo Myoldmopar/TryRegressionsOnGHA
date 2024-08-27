55554885de58e27c2440378342dd27db37e942dc
Edwin Lee <leeed2001@gmail.com>
2024-08-27 12:27:40 -0500

    Actually make the file

--- build_regression_bundle.py
+++ build_regression_bundle.py
@@ -6,10 +6,9 @@ with open('build/regressions/index.html', 'w') as f:
     f.write('<html><h4>Hello, world</h4></html>')
 
 if argv[1] == 'macos-14':
+    with open('build/summary.md', 'w') as md:
+        md.write("# Markdown Summary\n\nThis is a great tool")
     print("Regressions encountered!  They will be uploaded as an artifact on GitHub Actions")
     exit(1)
 
 print("No regressions found, great!")
-
-with open('build/summary.md', 'w') as md:
-    md.write("# Markdown Summary\n\nThis is a great tool")
