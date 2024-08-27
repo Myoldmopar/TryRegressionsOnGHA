283d0fc3dc7d9f68e7e58431b512199a8a07fd3b
Edwin Lee <leeed2001@gmail.com>
2024-08-27 12:34:53 -0500

    Nicer output

--- build_regression_bundle.py
+++ build_regression_bundle.py
@@ -7,7 +7,20 @@ with open('build/regressions/index.html', 'w') as f:
 
 if argv[1] == 'macos-14':
     with open('build/summary.md', 'w') as md:
-        md.write("# Markdown Summary\n\nThis is a great tool")
+        contents = """
+<details>
+  <summary>ESO Diffs</summary>
+  - Foo
+  - Bar
+  - Baz
+</details>
+
+<details>
+  <summary>EIO Diffs</summary>
+  - Hello
+  - World
+</details>"""
+        md.write(contents)
     print("Regressions encountered!  They will be uploaded as an artifact on GitHub Actions")
     exit(1)
 
