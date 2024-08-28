588673da9a7ac838cada9b7b0b9bc4be51640f2e
Edwin Lee <leeed2001@gmail.com>
2024-08-28 09:06:01 -0500

    Almost there

--- build_regression_summary.py
+++ build_regression_summary.py
@@ -3,11 +3,13 @@ with open('build/summary.md') as md:
 
 with open('build/summary.js', 'w') as js:
     js_contents = f"""
-github.rest.issues.createComment({{
-  issue_number: context.issue.number,
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  body: `{md_contents}`
-}})
+module.exports = ({{github, context}}) => {{    
+    github.rest.issues.createComment({{
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: `{md_contents}`
+    }})
+}}
 """
     js.write(js_contents)
