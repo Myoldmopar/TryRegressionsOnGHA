ef045d85b8c78c6e8fd421dc628c05b996d0083b
Edwin Lee <leeed2001@gmail.com>
2024-08-29 13:52:49 -0500

    Try fixing doc issues

--- build_regression_summary.py
+++ build_regression_summary.py
@@ -13,3 +13,6 @@ module.exports = ({{github, context}}) => {{
 }}
 """
     js.write(js_contents)
+
+
+# body: "### :warning: Regressions detected on ${{ matrix.os }} for commit ${{ github.sha }}\n\n ${{ env.R_STUFF }}\n\n - [View Results](https://github.com/NREL/EnergyPlus/actions/runs/${{ github.run_id }})\n - [Download Regressions](${{ steps.upload_regressions.outputs.artifact-url }})"
